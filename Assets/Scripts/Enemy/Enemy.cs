af5dffd0e5093ddc3ed3793a06711b81ca623b70
Stepan Nazar <64284448+StepanNazar@users.noreply.github.com>
2026-05-18 11:22:10 +0300

    enemy animation

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -93,7 +93,12 @@ public class Enemy : MonoBehaviour
             moveSpeed * Time.deltaTime
         );
 
-  
+        // Flip sprite based on movement direction
+        float movementDirection = _targetPosition.x - transform.position.x;
+        if (movementDirection > 0)
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        else if (movementDirection < 0)
+            transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
 
         float relativeDisatance = (transform.position - _targetPosition).magnitude;
         float stepDistance = Vector3.Distance(transform.position, _lastPosition);
